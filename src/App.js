e969f728faeb8824111a839f8415d88d2c076d98
Rudy <rudy.mejia@sacsa-ca.com>
2022-03-04 17:05:42 -0600

    Routering with an issue with Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Department } from "./Department"
+import { Employee } from "./Employee";
+import { Navigation } from "./Navigation";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
       <h3 className='m-3 d-flex justify-content-center'>
         React JS App
       </h3>
+
+      <Navigation/>
+      <Switch>
+        <Route path='/' component={Home} exact/>
+      </Switch>
+
     </div>
+    </BrowserRouter>
   );
 }
 
